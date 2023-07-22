6dae7856150fedeabc91d431f5742cd37c81e4a4
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-22 21:24:28 +0900

    漏れていたファイルの追加 + githubログインのリンクを追加

---
+++ db/migrate/20230722122206_create_user_ruby_methods.rb
@@ -0,0 +1,12 @@
+class CreateUserRubyMethods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_ruby_methods do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :ruby_method, null: false, foreign_key: true
+      t.text :memo
+      t.boolean :remembered, null: false
+
+      t.timestamps
+    end
+  end
+end
