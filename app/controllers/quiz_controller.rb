cbfb01dd4791de113f225c6213754555e2b41715
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-23 21:49:09 +0900

    showアクションを実装する

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -4,4 +4,19 @@ class QuizController < ApplicationController
   def new
     @ruby_method = RubyMethod.new
   end
+
+  def show
+    if params[:quiz_mode] == 'yes'
+      @ruby_method = RubyMethod.user_methods_and_module.user_remembered(current_user, remembered: true).sample
+      @user_ruby_method = @ruby_method.user_ruby_methods.first if @ruby_method.present?
+    elsif params[:quiz_mode] == 'no'
+      @ruby_method = RubyMethod.user_methods_and_module.user_remembered(current_user, remembered: false).sample
+      @user_ruby_method = @ruby_method.user_ruby_methods.first if @ruby_method.present?
+    else
+      all_methods = RubyMethod.includes([:ruby_module])
+      challenged_methods = current_user.user_methods
+      @ruby_method = all_methods.unchallenged_ruby_method(all_methods, challenged_methods)
+    end
+    @ruby_module = @ruby_method.ruby_module if @ruby_method.present?
+  end
 end
