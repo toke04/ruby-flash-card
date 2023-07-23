d298fb371d27a6a0e500e8243c44e65a8f7725f2
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-23 21:31:04 +0900

    showとindexで使用するヘルパーメソッドを作成

--- app/helpers/user_ruby_methods_helper.rb
+++ app/helpers/user_ruby_methods_helper.rb
@@ -1,2 +1,11 @@
 module UserRubyMethodsHelper
+  def convert_text(user_ruby_method)
+    return '知っているに移動' if user_ruby_method.remembered == false
+
+    '知らないに移動' if user_ruby_method.remembered == true
+  end
+
+  def module_name(user_ruby_method)
+    user_ruby_method.ruby_method.ruby_module.name
+  end
 end
