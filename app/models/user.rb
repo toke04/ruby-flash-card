8108807546ff55a286ea34aae74ef09cb99e0772
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-23 11:44:29 +0900

    githubログイン機能実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
 
   validates :uid, presence: true, uniqueness: { scope: :provider }
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:github]
+  devise :omniauthable, omniauth_providers: [:github]
+
+  def self.find_for_github_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      user.name = auth.info.name
+      user.image = auth.info.image
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_22_122206) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_23_023734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
