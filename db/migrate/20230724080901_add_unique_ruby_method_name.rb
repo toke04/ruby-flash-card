28a311cba06a880737ceee166968fa71c4173be6
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-24 17:15:07 +0900

    ruby_methodsテーブルのnameにユニーク制約を付与

---
+++ db/migrate/20230724080901_add_unique_ruby_method_name.rb
@@ -0,0 +1,5 @@
+class AddUniqueRubyMethodName < ActiveRecord::Migration[7.0]
+  def change
+    add_index :ruby_methods, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_24_080610) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_24_080901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_24_080610) do
     t.string "official_url", default: "https://docs.ruby-lang.org/ja/latest/library/_builtin.html", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_ruby_methods_on_name", unique: true
     t.index ["ruby_module_id"], name: "index_ruby_methods_on_ruby_module_id"
   end
 
