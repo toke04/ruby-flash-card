7914d7e47cb529a429e7318db301130991701501
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-24 18:23:00 +0900

    uidは英数字がstringで入るので、case_insensitiveオプションを付けて大文字小文字を区別しなくて良いよ、と明示する必要がある。また、先にオブジェクトを作成し、ユニークなものしか入らないか実証する必要がある

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
+    subject(:normal_user) { create(:normal_user) }
     it { is_expected.to have_many(:user_methods).through(:user_ruby_methods).source(:ruby_method)}
     it { is_expected.to have_many(:user_ruby_methods).dependent(:destroy)}
-    # it { is_expected.to validate_uniqueness_of(:uid).scoped_to(:provider) }
+    it { is_expected.to validate_uniqueness_of(:uid).scoped_to(:provider).case_insensitive }
   end
 
   describe '#validations' do
