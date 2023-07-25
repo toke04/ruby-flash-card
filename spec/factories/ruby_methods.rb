607e4a56e5c3200d8595526b5846057ee025803d
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-26 01:58:30 +0900

    モジュール名とメソッド名が違う場合のテストも追加する

--- spec/factories/ruby_methods.rb
+++ spec/factories/ruby_methods.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :zip_method, class: 'RubyMethod' do
+  factory :zip_method_of_array, class: 'RubyMethod' do
     name { 'zip' }
     official_url { 'https://docs.ruby-lang.org/ja/latest/method/Array/i/zip.html' }
     association :ruby_module, factory: :array_module
+  end
+
+  factory :merge_method_of_hash, class: 'RubyMethod' do
+    name { 'merge' }
+    official_url { 'https://docs.ruby-lang.org/ja/latest/method/Hash/i/merge.html' }
+    association :ruby_module, factory: :hash_module
   end
 end
