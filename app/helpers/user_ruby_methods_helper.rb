11e5facdd9f14136b76a5efe1450485404219c2a
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-25 14:47:27 +0900

    微妙な文言とスタイルを変更する

--- app/helpers/user_ruby_methods_helper.rb
+++ app/helpers/user_ruby_methods_helper.rb
@@ -2,9 +2,9 @@
 
 module UserRubyMethodsHelper
   def convert_text(user_ruby_method)
-    return '知っているに移動' if user_ruby_method.remembered == false
+    return '分からなかった' if user_ruby_method.remembered == false
 
-    '知らないに移動' if user_ruby_method.remembered == true
+    '分かっていた' if user_ruby_method.remembered == true
   end
 
   def module_name(user_ruby_method)
