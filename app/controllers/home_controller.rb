75f6f44447e1ca2c2e06dcaac5d1ce0da59bdbd8
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-25 19:32:24 +0900

    topページの説明を充実させる

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @message = 'Hello, world!'
     redirect_to quiz_new_path if user_signed_in?
   end
 
