987b8877ad4e655d77f41f1ced873f7d7f835013
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-26 15:02:05 +0900

    ユーザーのメソッド作成時の条件にremembered_trueを追加 + ハッシュのmergeメソッドも追加

--- spec/factories/user_ruby_methods.rb
+++ spec/factories/user_ruby_methods.rb
@@ -3,7 +3,23 @@
 FactoryBot.define do
   factory :user_zip_method, class: 'UserRubyMethod' do
     memo { 'レシーバーのインデックス番号に合わせて引数の配列を合体させて、配列を作成する' }
+    remembered { false }
     association :user, factory: :user
     association :ruby_method, factory: :zip_method_of_array
+
+    trait :remembered_true do
+      remembered { true }
+    end
+  end
+
+  factory :user_merge_method, class: 'UserRubyMethod' do
+    memo { 'レシーバーと引数のハッシュを合体させて、新しいハッシュを作成する' }
+    remembered { false }
+    association :user, factory: :user
+    association :ruby_method, factory: :merge_method_of_hash
+
+    trait :remembered_true do
+      remembered { true }
+    end
   end
 end
