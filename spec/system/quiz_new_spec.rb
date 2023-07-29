56160ed3732afb854668321206c91b99cfecf2f6
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-29 12:33:52 +0900

    クイズを解いた後のテストを追加する

--- spec/system/quiz_new_spec.rb
+++ spec/system/quiz_new_spec.rb
@@ -3,15 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Quiz new', type: :system do
-  let!(:user) { create(:user) }
-  let!(:zip_method_of_array) { create(:zip_method_of_array) }
-
-  before do
-    login_as(user)
-    visit quiz_new_path
-  end
-
   context '初めて利用した場合' do
+    let!(:user) { create(:user) }
+    let!(:zip_method_of_array) { create(:zip_method_of_array) }
+
+    before do
+      login_as(user)
+      visit quiz_new_path
+    end
+
     it '選択肢は無しで「クイズ start！」ボタンが表示されること' do
       expect(page).to have_content 'Ruby Quizへようこそ！'
       expect(page).to have_content 'クイズ START！'
@@ -27,7 +27,19 @@ RSpec.describe 'Quiz new', type: :system do
   end
 
   context '一回でもクイズを解いた場合' do
+    let!(:user) { create(:user) }
+    let!(:user_zip_method) { create(:user_zip_method, { user: }) }
+
+    before do
+      login_as(user)
+      visit quiz_new_path
+    end
+
     it '３つの選択肢が表示されること' do
+      expect(page).to have_content '選んだ条件からクイズが出題されます'
+      expect(page).to have_content '挑戦してないメソッドから出題する'
+      expect(page).to have_content '分からなかったメソッドから出題する'
+      expect(page).to have_content '分かっていたメソッドから出題する'
     end
   end
 end
