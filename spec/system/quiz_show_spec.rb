0a6a492428cc77f6992f912580b8fd50a114ed52
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-30 20:27:22 +0900

    describeでまとめられる所をまとめるよう変更した

--- spec/system/quiz_show_spec.rb
+++ spec/system/quiz_show_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Quiz show', type: :system, js: true do
-  context '「分かっているので次へ」を押した場合' do
+  describe 'クイズの最初の出題のテスト' do
     let!(:user) { create(:user) }
     let!(:zip_method_of_array) { create(:zip_method_of_array) }
     let!(:merge_method_of_hash) { create(:merge_method_of_hash) }
@@ -13,63 +13,58 @@ RSpec.describe 'Quiz show', type: :system, js: true do
       visit quiz_show_path
     end
 
-    # 出題条件はランダムになるので、メソッド名を確認条件には使えない
-    it '次の問題が表示されること' do
-      expect(page).to have_content 'Rubyフラッシュカード'
-      click_on '分かっているので次へ'
-      expect(page).to have_content '分かっているので次へ'
-      expect(page).to have_content '分からないので確認する'
-    end
-  end
-
-  context '「分からないので確認する」を押した場合' do
-    let!(:user) { create(:user) }
-    let!(:zip_method_of_array) { create(:zip_method_of_array) }
-    let!(:merge_method_of_hash) { create(:merge_method_of_hash) }
-
-    before do
-      login_as(user)
-      visit quiz_show_path
-      click_on('分からないので確認する')
-    end
-
-    it '公式サイトが表示されること' do
-      official_site_ifram = find('iframe[id=officialSite]')
-      Capybara.within_frame official_site_ifram do
-        expect(page).to have_content(/Ruby [3-9]\.[0-9] リファレンスマニュアル/)
+    context '「分かっているので次へ」を押した場合' do
+      it '次の問題が表示されること' do
+        expect(page).to have_content 'Rubyフラッシュカード'
+        click_on '分かっているので次へ'
+        expect(page).to have_content '分かっているので次へ'
+        expect(page).to have_content '分からないので確認する'
       end
     end
 
-    it 'オンラインエディターが表示されること' do
-      expect(page).to have_content 'コードを貼り付けて試す事ができます'
-    end
+    context '「分からないので確認する」を押した場合' do
+      before do
+        click_on('分からないので確認する')
+      end
 
-    it '「次の問題へ」を押すと、次の問題が出題されること' do
-      click_on '次の問題へ'
-      expect(page).to have_content 'Rubyフラッシュカード'
-      expect(page).to have_content '分かっているので次へ'
-      expect(page).to have_content '分からないので確認する'
-    end
+      it '公式サイトが表示されること' do
+        official_site_ifram = find('iframe[id=officialSite]')
+        Capybara.within_frame official_site_ifram do
+          expect(page).to have_content(/Ruby [3-9]\.[0-9] リファレンスマニュアル/)
+        end
+      end
 
-    it '公式サイトへ遷移するためのリンクが表示されること' do
-      expect(page).to have_link '公式サイトへアクセスして確認する', href: %r{^https://docs.ruby-lang.org/ja/latest/method.*}
-    end
+      it 'オンラインエディターが表示されること' do
+        expect(page).to have_content 'コードを貼り付けて試す事ができます'
+      end
 
-    it '「出題条件を変える」をクリックすると、設定画面へ遷移すること' do
-      click_on '出題条件を変える'
-      expect(page).to have_content '選んだ条件から出題されます'
-      expect(page).to have_content '挑戦してないメソッドから出題する'
-      expect(page).to have_content '分からなかったメソッドから出題する'
-      expect(page).to have_content '分かっていたメソッドから出題する'
-    end
+      it '「次の問題へ」を押すと、次の問題が出題されること' do
+        click_on '次の問題へ'
+        expect(page).to have_content 'Rubyフラッシュカード'
+        expect(page).to have_content '分かっているので次へ'
+        expect(page).to have_content '分からないので確認する'
+      end
 
-    it '「メソッド一覧へ」をクリックすると、メソッド一覧画面へ遷移すること' do
-      click_on 'メソッド一覧へ'
-      expect(page).to have_selector 'h1', text: 'メソッド一覧'
+      it '公式サイトへ遷移するためのリンクが表示されること' do
+        expect(page).to have_link '公式サイトへアクセスして確認する', href: %r{^https://docs.ruby-lang.org/ja/latest/method.*}
+      end
+
+      it '「出題条件を変える」をクリックすると、設定画面へ遷移すること' do
+        click_on '出題条件を変える'
+        expect(page).to have_content '選んだ条件から出題されます'
+        expect(page).to have_content '挑戦してないメソッドから出題する'
+        expect(page).to have_content '分からなかったメソッドから出題する'
+        expect(page).to have_content '分かっていたメソッドから出題する'
+      end
+
+      it '「メソッド一覧へ」をクリックすると、メソッド一覧画面へ遷移すること' do
+        click_on 'メソッド一覧へ'
+        expect(page).to have_selector 'h1', text: 'メソッド一覧'
+      end
     end
   end
 
-  describe 'メソッドにメモを書く場合', js: true do
+  describe 'メソッドにメモを書く場合' do
     let!(:user) { create(:user) }
     let!(:zip_method_of_array) { create(:zip_method_of_array) }
     let!(:user_merge_method) { create(:user_merge_method, { user: }) }
