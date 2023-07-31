e48dcf43c18f29bf55b8b87335f9a6879d230767
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-31 18:47:10 +0900

    テストの説明を分かりやすくした

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,17 +22,20 @@ RSpec.describe User, type: :model do
 
   describe '#find_for_github_oauth' do
     let(:user) { create(:user) }
-
-    it 'DBにユーザーが登録されてない(uidが無い)場合、新しいユーザーを作成すること' do
-      # userインスタンスはDBに保存されていない
-      new_user = build(:user)
-      auth_hash = { provider: new_user.provider, uid: new_user.uid, info: { name: new_user.name, image: new_user.image } }
-      expect { described_class.find_for_github_oauth(auth_hash) }.to change(described_class, :count).from(0).to(1)
+    context 'DBにproviderとuidの組み合わせで登録されてない情報が渡ってくる場合' do
+      it '新しいユーザーを作成すること' do
+        # userインスタンスはDBに保存されていない
+        new_user = build(:user)
+        auth_hash = { provider: new_user.provider, uid: new_user.uid, info: { name: new_user.name, image: new_user.image } }
+        expect { described_class.find_for_github_oauth(auth_hash) }.to change(described_class, :count).from(0).to(1)
+      end
     end
 
-    it 'すでにユーザーのデータがDBに保存されている場合、同じユーザーが返ってくること' do
-      auth_hash = { provider: user.provider, uid: user.uid, info: { name: user.name, image: user.image } }
-      expect(described_class.find_for_github_oauth(auth_hash)).to eq user
+    context 'すでにDBにproviderとuidの組み合わせが登録されている場合' do
+      it '同じユーザー情報が返ってくること' do
+        auth_hash = { provider: user.provider, uid: user.uid, info: { name: user.name, image: user.image } }
+        expect(described_class.find_for_github_oauth(auth_hash)).to eq user
+      end
     end
   end
 end
