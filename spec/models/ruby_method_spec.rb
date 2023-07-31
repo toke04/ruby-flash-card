12cea76db85a49087cfdf37b3095258b5c1848b8
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-07-31 11:57:36 +0900

    RubyMethodモデルのassociationとvalidationテストを実装する

--- spec/models/ruby_method_spec.rb
+++ spec/models/ruby_method_spec.rb
@@ -3,21 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe RubyMethod, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:user_ruby_methods).dependent(:destroy) }
+    it { is_expected.to belong_to(:ruby_module) }
+  end
+
   describe '#validations' do
+    subject(:zip_method_of_array) { create(:zip_method_of_array) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name).scoped_to(:ruby_module_id) }
+    it { is_expected.to validate_presence_of(:official_url) }
+
     it 'ファクトリが有効であること' do
       expect(build(:zip_method_of_array)).to be_valid
-    end
-
-    it 'nameが無ければ、レコード作成が無効であること' do
-      zip_method_of_array = build(:zip_method_of_array, name: nil)
-      zip_method_of_array.valid?
-      expect(zip_method_of_array.errors[:name]).to include('を入力してください')
-    end
-    it '重複したnameが存在するなら、レコード作成が無効であること' do
-      create(:zip_method_of_array)
-      zip_method_of_array = build(:zip_method_of_array)
-      zip_method_of_array.valid?
-      expect(zip_method_of_array.errors[:name]).to include('はすでに存在します')
     end
   end
 end
