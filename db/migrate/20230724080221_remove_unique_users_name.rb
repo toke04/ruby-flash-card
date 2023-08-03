2350ace0b4317f5b8a76d2577613955f4ee904ac
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-08-03 13:04:47 +0900

    ruby_methodsテーブルのnameカラムのuniqueインデックスの不具合を修正する

--- db/migrate/20230724080221_remove_unique_users_name.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveUniqueUsersName < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :users, column: :name
-  end
-end
---
+++ db/migrate/20230803035304_delete_name_uniq_index_from_ruby_method.rb
@@ -0,0 +1,5 @@
+class DeleteNameUniqIndexFromRubyMethod < ActiveRecord::Migration[7.0]
+  def change
+    remove_index :ruby_methods, column: :name
+  end
+end
