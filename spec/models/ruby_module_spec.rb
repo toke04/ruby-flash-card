daccb63f08e3494b1f14049108a02d0cba66bd1a
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-08-05 14:40:11 +0900

    rspecの文言の微調整と、itの場所を変更する

--- spec/models/ruby_module_spec.rb
+++ spec/models/ruby_module_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe RubyModule, type: :model do
-  it 'ファクトリが有効であること' do
-    expect(build(:array_module)).to be_valid
-  end
-
   describe 'validations' do
     subject(:array_module) { build(:array_module) }
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
+
+    it 'ファクトリが有効であること' do
+      expect(build(:array_module)).to be_valid
+    end
   end
 
   describe 'associations' do
