e64275c566288a44d609918be842509541919462
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-08-10 21:35:31 +0900

    bin/lintを通す

--- app/javascript/components/OnlineEditor.tsx
+++ app/javascript/components/OnlineEditor.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import Editor from "react-simple-code-editor";
-import {highlight, languages} from "prismjs";
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-ruby';
-import 'prismjs/themes/prism.css';
-import "../src/OnlineEditor.css"
+import Editor from 'react-simple-code-editor'
+import { highlight, languages } from 'prismjs'
+import 'prismjs/components/prism-clike'
+import 'prismjs/components/prism-ruby'
+import 'prismjs/themes/prism.css'
+import '../src/OnlineEditor.css'
 
 export const OnlineEditor = () => {
   const [showEditor, setShowEditor] = useState(true)
@@ -15,25 +15,28 @@ export const OnlineEditor = () => {
     return codeResult.match(/Error/) ? 'text-error' : 'text-success'
   }
 
-  const hightlightWithLineNumbers = (input:string, language:string) =>
+  const hightlightWithLineNumbers = (input: string, language: string) =>
     highlight(input, language)
-      .split("\n")
-      .map((line, index) => `<span class='editorLineNumber'>${index + 1}</span>${line}`)
-      .join("\n");
+      .split('\n')
+      .map(
+        (line, index) =>
+          `<span class='editorLineNumber'>${index + 1}</span>${line}`
+      )
+      .join('\n')
 
-  const setCorrectErrorMessage = (wrongErrorMessage:string) => {
+  const setCorrectErrorMessage = (wrongErrorMessage: string) => {
     // 以下のようなエラーが発生するので、それを取得する「"Error: eval:5:in `fetch_values': key not found: \"bird\" (KeyError) eval:5:in `<main>'"」
-    const regex = /Error: eval:(\d+):in `([^']+)'(.*)/;
-    const match = regex.exec(wrongErrorMessage.toString());
+    const regex = /Error: eval:(\d+):in `([^']+)'(.*)/
+    const match = regex.exec(wrongErrorMessage.toString())
     if (match && match.length === 4) {
-      const wrongLineNumber = parseInt(match[1]);
-      const causeOfError = match[2];
-      const errorMessage = match[3];
-      const correctLineNumber = wrongLineNumber - 1; // ruby.wasmの仕様で、行番号が1ズレてしまうので、修正する
-      const correctErrorMessage = `Error occurred on line ${correctLineNumber} : \`${causeOfError}'${errorMessage}`;
-      setCodeResult(correctErrorMessage);
+      const wrongLineNumber = parseInt(match[1])
+      const causeOfError = match[2]
+      const errorMessage = match[3]
+      const correctLineNumber = wrongLineNumber - 1 // ruby.wasmの仕様で、行番号が1ズレてしまうので、修正する
+      const correctErrorMessage = `Error occurred on line ${correctLineNumber} : \`${causeOfError}'${errorMessage}`
+      setCodeResult(correctErrorMessage)
     } else {
-      console.log(`Not correctly acquired by regular expression：${match}`);
+      console.log(`Not correctly acquired by regular expression：${match}`)
     }
   }
 
@@ -71,19 +74,18 @@ export const OnlineEditor = () => {
           <p className="font-bold">貼り付けたコードの最終行を出力します</p>
           <Editor
             value={rubyCode}
-            onValueChange={code => setRubyCode(code)}
-            highlight={code => hightlightWithLineNumbers(code, languages.rb)}
+            onValueChange={(rubyCode) => setRubyCode(rubyCode)}
+            highlight={(rubyCode) => hightlightWithLineNumbers(rubyCode, languages.rb)}
             padding={10}
-            placeholder={'text = "ruby love"\n' +
-              'text.upcase'}
+            placeholder={'text = "ruby love"\n' + 'text.upcase'}
             textareaId="codeArea"
             className="editor rounded"
             style={{
               fontFamily: '"Fira code", "Fira Mono", monospace',
               fontSize: 18,
               outline: 0,
-              border: "1px solid",
-              minHeight: "100px"
+              border: '1px solid',
+              minHeight: '100px',
             }}
           />
           <button
