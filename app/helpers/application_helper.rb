a06e12f0f94e2e1800477e5020ea484bfe2576b4
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-09-09 17:54:35 +0900

    ロゴ画像をsvg画像に変更する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@
 
 module ApplicationHelper
   include MetaTagsHelper
+
+  def embedded_svg(filename, options={})
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    doc.to_html.html_safe
+  end
 end
