b406fa526b0e9f3ec642d066952b5c5d1165f1f6
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-09-09 12:45:17 +0900

    エディターを常に表示するよう、修正する

--- app/javascript/components/OnlineEditor.tsx
+++ app/javascript/components/OnlineEditor.tsx
@@ -8,7 +8,6 @@ import 'prismjs/themes/prism.css'
 import '../src/OnlineEditor.css'
 
 export const OnlineEditor = () => {
-  const [showEditor, setShowEditor] = useState(true)
   const [rubyCode, setRubyCode] = useState('')
   const [codeResult, setCodeResult] = useState('')
   useHotkeys(
@@ -72,53 +71,43 @@ export const OnlineEditor = () => {
       <p className="mt-7 font-bold text-2xl official-url-title relative">
         コードを実行して試す
       </p>
-      <div className="flex justify-end">
-        <button
-          className="btn btn-sm w-40 h-10 border-2 border-slate-200	bg-white hover:bg-white-100 rounded"
-          onClick={() => setShowEditor(!showEditor)}
-        >
-          {showEditor ? 'エディターを閉じる' : 'エディターを開く'}
-        </button>
-      </div>
-      {showEditor && (
-        <div className="mb-6 className={`w-full h-96`}">
-          <p className="font-bold">
-            貼り付けたコードの最終行の戻り値を出力します
-          </p>
-          <Editor
-            value={rubyCode}
-            onValueChange={(rubyCode) => setRubyCode(rubyCode)}
-            highlight={(rubyCode) =>
-              hightlightWithLineNumbers(rubyCode, languages.rb)
-            }
-            padding={10}
-            placeholder={'text = "ruby love"\n' + 'text.upcase'}
-            textareaId="codeArea"
-            className="editor rounded"
-            style={{
-              fontFamily: '"Fira code", "Fira Mono", monospace',
-              fontSize: 18,
-              outline: 0,
-              border: '1px solid',
-              minHeight: '100px',
-            }}
-          />
-          <div className="flex">
-            <button
-              onClick={execCode}
-              className="btn btn-sm w-48 h-10 mt-3 mb-7　border-2 border-slate-200 bg-white hover:bg-white-100 rounded code-exec-button"
-            >
-              実行する(⌘+Enter)
-            </button>
-          </div>
-          <p className="font-bold mt-5 mb-1">実行結果</p>
-          <div className="mockup-code">
-            {codeResult && (
-              <p className={`px-2 ${codeColor()}`}>{codeResult}</p>
-            )}
-          </div>
+      <div className="mb-6 className={`w-full h-96`}">
+        <p className="font-bold mt-3">
+          貼り付けたコードの最終行の戻り値を出力します
+        </p>
+        <Editor
+          value={rubyCode}
+          onValueChange={(rubyCode) => setRubyCode(rubyCode)}
+          highlight={(rubyCode) =>
+            hightlightWithLineNumbers(rubyCode, languages.rb)
+          }
+          padding={10}
+          placeholder={'text = "ruby love"\n' + 'text.upcase'}
+          textareaId="codeArea"
+          className="editor rounded"
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 18,
+            outline: 0,
+            border: '1px solid',
+            minHeight: '100px',
+          }}
+        />
+        <div className="flex">
+          <button
+            onClick={execCode}
+            className="btn btn-sm w-48 h-10 mt-3 mb-7　border-2 border-slate-200 bg-white hover:bg-white-100 rounded code-exec-button"
+          >
+            実行する(⌘+Enter)
+          </button>
         </div>
-      )}
+        <p className="font-bold mt-5 mb-1">実行結果</p>
+        <div className="mockup-code">
+          {codeResult && (
+            <p className={`px-2 ${codeColor()}`}>{codeResult}</p>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
