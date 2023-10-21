1e6e51a7c5076ef30947f242f7355e8df73bf707
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-10-21 13:59:22 +0900

    seeds.rbの繰り返している処理の削除 + 冗長な名前の削除を行う

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,26 +20,18 @@ end
 ruby_module_ids = RubyModule.all.order(id: 'ASC').pluck(:id)
 
 # rubocop:disable Layout/LineLength
-enumerable_methods = %w[collect_concat each_slice each_with_object entries filter_map find find_all flat_map grep grep_v group_by inject max_by min_by minmax minmax_by partition reduce reject slice_after slice_before slice_when sort_by take take_while tally]
+enumerables = %w[collect_concat each_slice each_with_object entries filter_map find find_all flat_map grep grep_v group_by inject max_by min_by minmax minmax_by partition reduce reject slice_after slice_before slice_when sort_by take take_while tally]
 
-array_methods = %w[append assoc at clear clone collect compact concat count delete delete_at delete_if difference dig drop drop_while dup fetch fill filter find_index flatten hash index insert intersection join keep_if max min minmax pop prepend product push reject replace rindex rotate sample select shift shuffle size slice sort sum take take_while transpose union uniq unshift values_at]
+arrays = %w[append assoc at clear clone collect compact concat count delete delete_at delete_if difference dig drop drop_while dup fetch fill filter find_index flatten hash index insert intersection join keep_if max min minmax pop prepend product push reject replace rindex rotate sample select shift shuffle size slice sort sum take take_while transpose union uniq unshift values_at]
 
-hash_methods = %w[assoc clear clone compact delete delete_if dig dup except fetch fetch_values filter flatten invert keep_if key keys length merge rehash reject replace select shift size slice store transform_keys transform_values update values values_at]
+hashs = %w[assoc clear clone compact delete delete_if dig dup except fetch fetch_values filter flatten invert keep_if key keys length merge rehash reject replace select shift size slice store transform_keys transform_values update values values_at]
 
-string_methods = %w[byteindex capitalize casecmp center chars chr concat count delete delete_prefix delete_suffix downcase each_line gsub index insert length lines ljust match next partition prepend replace rindex rjust rpartition scan slice split squeeze sub swapcase tr tr_s upcase]
+strings = %w[byteindex capitalize casecmp center chars chr concat count delete delete_prefix delete_suffix downcase each_line gsub index insert length lines ljust match next partition prepend replace rindex rjust rpartition scan slice split squeeze sub swapcase tr tr_s upcase]
 
-numeric_methods = %w[abs abs2 ceil div divmod quo remainder round truncate]
+numerics = %w[abs abs2 ceil div divmod quo remainder round truncate]
 
-range_methods = %w[begin end size]
+ranges = %w[begin end size]
 # rubocop:enable Layout/LineLength
-
-def create_url(module_name, method_name)
-  if Net::HTTP.get_response(URI.parse("https://docs.ruby-lang.org/ja/latest/method/#{module_name}/i/#{method_name}.html")).code == '200'
-    "https://docs.ruby-lang.org/ja/latest/method/#{module_name}/i/#{method_name}.html"
-  else
-    "https://docs.ruby-lang.org/ja/latest/class/#{module_name}.html#I_#{method_name.upcase.slice(/[^?]+/)}--3F"
-  end
-end
 
 def create_method(method_name, module_id, module_name)
   RubyMethod.create([
@@ -49,26 +41,16 @@ def create_method(method_name, module_id, module_name)
                     ])
 end
 
-enumerable_methods.each do |method_name|
-  create_method(method_name, ruby_module_ids[0], module_names[0])
+def create_url(module_name, method_name)
+  if Net::HTTP.get_response(URI.parse("https://docs.ruby-lang.org/ja/latest/method/#{module_name}/i/#{method_name}.html")).code == '200'
+    "https://docs.ruby-lang.org/ja/latest/method/#{module_name}/i/#{method_name}.html"
+  else
+    "https://docs.ruby-lang.org/ja/latest/class/#{module_name}.html#I_#{method_name.upcase.slice(/[^?]+/)}--3F"
+  end
 end
 
-array_methods.each do |method_name|
-  create_method(method_name, ruby_module_ids[1], module_names[1])
-end
-
-hash_methods.each do |method_name|
-  create_method(method_name, ruby_module_ids[2], module_names[2])
-end
-
-string_methods.each do |method_name|
-  create_method(method_name, ruby_module_ids[3], module_names[3])
-end
-
-numeric_methods.each do |method_name|
-  create_method(method_name, ruby_module_ids[4], module_names[4])
-end
-
-range_methods.each do |method_name|
-  create_method(method_name, ruby_module_ids[5], module_names[5])
+[enumerables, arrays, hashs, strings, numerics, ranges].each_with_index do |methods, index|
+  methods.each do |method_name|
+    create_method(method_name, ruby_module_ids[index], module_names[index])
+  end
 end
